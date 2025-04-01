319766ac248151cfdca2d97764c82a66871b5f27
JohnS300 <johnsofokleous@gmail.com>
2025-04-01 23:27:48 +0300

    Changed filenaming convention to use a hash value generated for uniqeness

--- excercises/qrGenerator.py
+++ excercises/qrGenerator.py
@@ -1,9 +1,13 @@
 import os
+import hashlib
 import qrcode
 from PIL import Image
 
 def qrGenerator():
     data = input("Enter anything to generate QR code : ")
+
+    hash_val = hashlib.md5(data.encode()).hexdigest()[:6]
+
     qr = qrcode.QRCode(version=3, box_size=8, border=4)
     qr.add_data(data)
     qr.make(fit=True)
@@ -13,7 +17,8 @@ def qrGenerator():
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    file_path = os.path.join(folder,'qr_code.png')
+    file_name = f"qr_code_{hash_val}.png"
+    file_path = os.path.join(folder,file_name)
     image.save(file_path)
     print(f'QR code generated in {file_path}')
 
