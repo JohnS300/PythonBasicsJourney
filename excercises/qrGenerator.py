bd9a0c748c9aeaf90b047013c98c6af5b121ebf3
JohnS300 <johnsofokleous@gmail.com>
2025-04-10 23:59:20 +0300

    Increasing modularity

--- excercises/qrGenerator.py
+++ excercises/qrGenerator.py
@@ -4,6 +4,21 @@ import pyperclip
 import qrcode # type: ignore
 from PIL import Image # type: ignore
 
+def clipboard(x):
+    pyperclip.copy(x)
+    print("File path copied to clipboard.")
+
+def encode(x):
+    hash_val = hashlib.md5(x.encode()).hexdigest()[:6]
+    return hash_val
+
+def imageCreation(x):
+    qr = qrcode.QRCode(version=3, box_size=8, border=4)
+    qr.add_data(x)
+    qr.make(fit=True)
+    image = qr.make_image(fill="Black", back_color="white")
+    return image
+
 def qrGenerator():
 
     while True:
@@ -12,26 +27,19 @@ def qrGenerator():
             print('No input. Please enter information to generate a QR code')
         else:
             break
-    
 
-    hash_val = hashlib.md5(data.encode()).hexdigest()[:6]
-
-    qr = qrcode.QRCode(version=3, box_size=8, border=4)
-    qr.add_data(data)
-    qr.make(fit=True)
-    image = qr.make_image(fill="Black", back_color="white")
+    image = imageCreation(data)
 
     folder = 'QRcodes'
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    file_name = f"qr_code_{hash_val}.png"
+    file_name = f"qr_code_{encode(data)}.png"
     file_path = os.path.join(folder,file_name)
     image.save(file_path)
     print(f'QR code generated in {file_path}')
 
-    pyperclip.copy(file_path)
-    print("File path copied to clipboard.")
+    clipboard(file_path)
     
     #Image.open(file_path).show()
 
